e0d286ba6306a4c67372e98152032c930d6efd13
kaliloulah1155 <ibrahim.konate@ngser.com>
2023-12-10 18:31:58 +0000

    add category

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,8 +1,5 @@
 <template>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" />
     <div class="dashboard ma-4">
         <v-banner lines="one" color="warning">
             <template v-slot:text>
@@ -13,7 +10,7 @@
             </template>
         </v-banner>
         <v-container class="my-5">
-            
+
             <v-card flat>
                 <v-card-title class="d-flex align-center pe-2">
                     <v-icon icon="view_list"></v-icon>&nbsp;
@@ -23,25 +20,27 @@
                         label="Rechercher une catégorie" single-line flat hide-details variant="solo-filled"></v-text-field>
                 </v-card-title>
                 <v-divider class="mt-5"></v-divider>
-                <v-data-table v-model:search="search" :headers="headers" :items="categories"
-                    :sort-by="[{ key: 'libelle', order: 'asc' }]"
-                        items-per-page="5"
-                        server-items-length="5"
-                    >
-                    <template v-slot:item.actions="{item}">
-                        <v-icon
-                        size="small"
-                        class="me-2"
-                        @click="editItem(item)"
-                        >
-                           edit_note
+                <v-data-table v-model:search="search" :headers="headers" :items="categories" 
+                :loading="categorieLength >0 ? false:true"
+                    :sort-by="[{ key: 'libelle', order: 'asc' }]" items-per-page="10">
+                      <template v-slot:item.statut="{item}">
+                            <v-chip
+                            :color="item.statut == 1 ? 'green':'red'"
+                            :text="item.statut ==1 ? 'Activée':'Desactivée'"
+                            class="text-uppercase"
+                            label
+                            size="small"
+                            >
+                            </v-chip>
+                    </template>
+                 
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon size="small" class="me-2" @click="editItem(item)">
+                            edit_note
                         </v-icon>
-                        <v-icon
-                        size="small"
-                        @click="deleteItem(item)"
-                        >
-                                delete
-                            </v-icon>
+                        <v-icon size="small" @click="deleteItem(item)">
+                            delete
+                        </v-icon>
 
                     </template>
                 </v-data-table>
@@ -63,10 +62,10 @@ export default {
                 sortable: false,
                 key: 'libelle'
             },
-            { title: "Calories", key: "calories" },
-            { title: "Fat (g)", key: "fat" },
-            { title: "Carbs (g)", key: "carbs" },
-            { title: "Protein (g)", key: "protein" },
+            { title: "Parent", key: "parent" },
+            { title: "Code", key: "code" },
+            { title: "Position", key: "position" },
+            { title: "Statut", key: "statut" },
             { title: "Actions", key: "actions", sortable: false }
         ],
         categories: []
@@ -78,109 +77,45 @@ export default {
         initialize() {
             this.categories = [
                 {
-                    libelle: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
+                    libelle: "Frozen Yogurt",
+                    parent: "",
+                    code: "FRZ",
+                    position: 2,
+                    statut: 1,
                 },
                 {
                     libelle: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
+                    parent: "",
+                    code: "ICE",
+                    position: 2,
+                    statut: 0,
                 },
                 {
                     libelle: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
+                    parent: "ICE",
+                    code: "ICE",
+                    position: 2,
+                    statut: 1,
                 },
-                {
-                    libelle: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                },
-                {
-                    libelle: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                },
-                {
-                    libelle: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                },
-                {
-                    libelle: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                },
-                {
-                    libelle: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                },
-                {
-                    libelle: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                },
-                {
-                    libelle: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                },
-                {
-                    libelle: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                },
-                 {
-                    libelle: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                },
-                 {
-                    libelle: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                },
+
             ]
         },
-        editItem(item){
+        editItem(item) {
             console.log(item)
         },
-        deleteItem(item){
+        deleteItem(item) {
             console.log(item)
         }
+    },
+    computed:{
+        categorieLength() {
+            return this.categories.length;
+        },
     }
 }
 </script>
 <style scoped>
-.v-pagination{
+.v-pagination {
     background: rgba(12, 185, 245, 0.8);
-  }
+}
 </style>
