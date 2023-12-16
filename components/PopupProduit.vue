90b6601ad411378e70c1efcd23f93751e3d141cc
kaliloulah1155 <ibrahim.konate@ngser.com>
2023-12-16 10:30:05 +0000

    UI Produit

---
+++ components/PopupProduit.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-dialog width="500" transition="dialog-bottom-transition">
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" text="Ajouter" variant="flat" color="blue-lighten-4" size="small"></v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card title="Ajouter un nouveau produit">
+        <v-card-text>
+          <v-form class="px-3" ref="form">
+            <v-text-field label="Libellé" color="primary" clearable variant="outlined"
+                                              v-model="editedItem.libelle" :rules="inputRules"></v-text-field>
+                                          <v-text-field label="Code" class="mt-2" color="primary" clearable
+                                              variant="outlined" v-model="editedItem.code"></v-text-field>
+                                          <v-text-field label="Prix d'achat" class="mt-2" color="primary" clearable
+                                              variant="outlined" v-model="editedItem.buying_price"
+                                              :rules="digitRules"></v-text-field>
+                                           <v-text-field label="Prix de vente" class="mt-2" color="primary" clearable
+                                                  variant="outlined" v-model="editedItem.selling_price"
+                                                  :rules="digitRules"></v-text-field>
+                                               <v-select label="Fournisseur" class="mt-2" color="primary" variant="outlined"
+                                                      v-model="editedItem.fournisseur" :items="fournisseurs" item-title="libelle"
+                                                      item-value="id" return-object></v-select>
+                                               <v-select label="Catégorie" class="mt-2" color="primary" variant="outlined"
+                                                  v-model="editedItem.categorie" :items="categories" item-title="libelle"
+                                                  item-value="id" return-object></v-select>
+                                          <v-file-input label="Image" v-model="image" accept="image/*" show-size counter
+                                              variant="outlined"></v-file-input>
+                                          <v-text-field label="Quantité" class="mt-2" color="primary" clearable
+                                                  variant="outlined" v-model="editedItem.quantite"
+                                                  :rules="digitRules"></v-text-field>
+
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text="FERMER" variant="elevated" @click="isActive.value = false" size="small"></v-btn>
+          <v-btn color="green-darken-3" variant="elevated" size="small" :loading="loading"
+            @click.prevent="submit">Ajouter</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+  <v-snackbar v-model="snackbar">
+    {{ text }}
+    <template v-slot:actions>
+      <v-btn color="pink" variant="text" @click="snackbar = false">
+        Fermer
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    snackbar: false,
+    text: "message du snackbar",
+    image:null,
+    editedItem: {
+      id: 0,
+      libelle: "",
+      code: "",
+      buying_price: 0,
+      selling_price: 0,
+      fournisseur: "",
+      categorie: "",
+      image: null,
+      quantite: 0
+    },
+    loading: false,
+      categories: [
+      { libelle: "Lait", id: 1 },
+      { libelle: 'Crème', id: 2 },
+      { libelle: 'Lotion', id: 3 },
+    ],
+    fournisseurs: [
+      { libelle: "Awa", id: 1 },
+      { libelle: 'Moye', id: 2 },
+      { libelle: 'Autre', id: 3 },
+    ],
+     inputRules: [
+      v => (v && v.length >= 3) || "La longueur minimale est de 3 caractères"
+    ],
+    digitRules: [
+      v => (v && /^\d+$/.test(v)) || 'Entrez des chiffres',
+    ],
+    // Select parent
+
+  }),
+  methods: {
+    async submit() {
+      this.loading = true;
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+        this.$emit('saveItem', this.editedItem);
+      }
+      this.loading = false;
+    },
+
+    isLeapYear(year) {
+      return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    },
+    isValidDate(day, month, year) {
+      const daysInMonth = [31, this.isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      return month >= 1 && month <= 12 && day >= 1 && day <= daysInMonth[month - 1];
+    },
+    isDateValid(v) {
+      if (!v || !/^\d{2}\/\d{2}\/\d{4}$/.test(v)) {
+        return 'Entrez une date valide au format DD/MM/YYYY';
+      }
+
+      const [day, month, year] = v.split('/').map(Number);
+
+      if (!this.isValidDate(day, month, year)) {
+        return 'Entrez une date valide avec le nombre correct de jours et de mois.';
+      }
+
+      return true;
+    },
+
+  },
+};
+</script>
+
+
+<style scoped></style>
--- pages/produits/index.vue
+++ pages/produits/index.vue
@@ -1,11 +1,301 @@
 <template>
     <div class="dashboard ma-4">
-        <h1 class="text-subtitle-1 text-grey">Produits</h1>
-        <v-container class="my-5"></v-container>
+        <NuxtPage title="Employés" />
+        <v-banner lines="one" color="warning">
+            <template v-slot:text>
+                <h1 class="text-subtitle-1 text-grey">Produits</h1>
+            </template>
+            <template v-slot:actions>
+                 <popup-produit @saveItem="getItem" /> 
+            </template>
+        </v-banner>
+        <v-container class="my-5">
+            <v-card flat>
+                <v-card-title class="d-flex align-center pe-2">
+                    <v-icon icon="view_list"></v-icon>&nbsp;
+                    Liste des produits
+                    <v-spacer></v-spacer>
+                    <v-text-field v-model="search" prepend-inner-icon="search" density="compact"
+                        label="Rechercher un produit" single-line flat hide-details variant="solo-filled"></v-text-field>
+                </v-card-title>
+                <v-divider class="mt-5"></v-divider>
+                <v-data-table v-model:search="search" :headers="headers" :items="produits"
+                    :loading="produitLength > 0 ? false : true" :sort-by="[{ key: 'libelle', order: 'asc' }]"
+                    items-per-page="10">
+
+                    <template v-slot:top>
+                        <v-spacer></v-spacer>
+                        <v-dialog v-model="dialog" max-width="500px">
+                            <v-card>
+                                <v-card-title>
+                                    <span class="text-h5">{{ formTitle }}</span>
+                                </v-card-title>
+                                <v-card-text>
+                                    <v-form class="px-3" ref="form">
+                                        <v-text-field label="Libellé" color="primary" clearable variant="outlined"
+                                            v-model="editedItem.libelle" :rules="inputRules"></v-text-field>
+                                        <v-text-field label="Code" class="mt-2" color="primary" clearable
+                                            variant="outlined" v-model="editedItem.code"></v-text-field>
+                                        <v-text-field label="Prix d'achat" class="mt-2" color="primary" clearable
+                                            variant="outlined" v-model="editedItem.buying_price"
+                                            :rules="digitRules"></v-text-field>
+                                         <v-text-field label="Prix de vente" class="mt-2" color="primary" clearable
+                                                variant="outlined" v-model="editedItem.selling_price"
+                                                :rules="digitRules"></v-text-field>
+                                             <v-select label="Fournisseur" class="mt-2" color="primary" variant="outlined"
+                                                    v-model="editedItem.fournisseur" :items="fournisseurs" item-title="libelle"
+                                                    item-value="id" return-object></v-select>
+                                             <v-select label="Catégorie" class="mt-2" color="primary" variant="outlined"
+                                                v-model="editedItem.categorie" :items="categories" item-title="libelle"
+                                                item-value="id" return-object></v-select>
+                                        <v-file-input label="Image" v-model="image" accept="image/*" show-size counter
+                                            variant="outlined"></v-file-input>
+                                        <v-text-field label="Quantité" class="mt-2" color="primary" clearable
+                                                variant="outlined" v-model="editedItem.quantite"
+                                                :rules="digitRules"></v-text-field>
+                                    </v-form>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn variant="elevated" size="small" @click="close">
+                                        Annuler
+                                    </v-btn>
+                                    <v-btn color="green-darken-3" variant="elevated" size="small" @click="save">
+                                        Valider
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                        <v-dialog v-model="dialogDelete" max-width="500px">
+                            <v-card max-height="500px">
+                                <div class="text-center mt-4">
+                                    <v-icon icon="delete_forever" color="red" size="large"></v-icon>
+                                </div>
+                                <v-card-title class="text-h5 text-center">&Ecirc;tes-vous s&ucirc;re de supprimer
+                                    ?</v-card-title>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn variant="elevated" size="x-small" @click="closeDelete">Annuler</v-btn>
+                                    <v-btn color="red-darken-3" size="x-small" variant="elevated"
+                                        @click="deleteItemConfirm">Confirmer</v-btn>
+                                    <v-spacer></v-spacer>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </template>
+                    <template v-slot:item.image="{ item }">
+                        <v-card class="my-2" elevation="2" rounded>
+                            <v-img :src="item.image ? `${item.image}` : '/img/profil.png'" height="64" cover></v-img>
+                        </v-card>
+                    </template>
+
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon size="small" class="me-2" @click="editItem(item)">
+                            edit_note
+                        </v-icon>
+                        <v-icon size="small" @click="deleteItem(item)">
+                            delete
+                        </v-icon>
+                    </template>
+
+                </v-data-table>
+            </v-card>
+        </v-container>
     </div>
 </template>
-<script setup>
-definePageMeta({
-    layout: 'master'
-})
+<script>
+export default {
+    setup() {
+        definePageMeta({
+            layout: 'master'
+        })
+        return {}
+    },
+    data: () => ({
+        dialog: false,
+        dialogDelete: false,
+        search: "",
+        image: null,
+        headers: [
+            { title: "Libellé", align: "start", key: 'libelle' },
+            { title: "Code", key: "code" },
+            { title: "Prix d'achat", key: "buying_price" },
+            { title: "Prix de vente", key: "selling_price" },
+            { title: "Image", key: "image" },
+            { title: "Fournisseurs", key: "fournisseur" },
+            { title: "Catégories", key: "categorie" },
+            { title: "Quantité", key: "quantite" },
+            { title: "Actions", key: "actions", sortable: false }
+        ],
+        inputRules: [
+            v => (v && v.length >= 3) || "La longueur minimale est de 3 caractères"
+        ],
+        digitRules: [
+            v => (v && /^\d+$/.test(v) ) || 'Entrez des chiffres',
+        ],
+        categories: [
+            { libelle: "Lait", id: 1 },
+            { libelle: 'Crème', id: 2 },
+            { libelle: 'Lotion', id: 3 },
+        ],
+        fournisseurs: [
+            { libelle: "Awa", id: 1 },
+            { libelle: 'Moye', id: 2 },
+            { libelle: 'Autre', id: 3 },
+        ],
+        produits: [],
+        editedIndex: -1,
+        editedItem: {
+            id: 0,
+            libelle: "",
+            code: "",
+            buying_price: 0,
+            selling_price: 0,
+            fournisseur: "",
+            categorie: "",
+            image: null,
+            quantite: 0
+
+        },
+        defaultItem: {
+            id: 0,
+            libelle: "",
+            code: "",
+            buying_price: 0,
+            selling_price: 0,
+            fournisseur: "",
+            categorie: "",
+            image: null,
+            quantite: 0
+        },
+    }),
+
+    watch: {
+        dialog(val) {
+            val || this.close()
+        },
+        dialogDelete(val) {
+            val || this.closeDelete()
+        }
+    },
+    created() {
+        this.initialize()
+    },
+    methods: {
+        initialize() {
+            this.produits = [
+                {
+                    id: 1,
+                    libelle: "ICE Milano and ice L",
+                    code: "ABZ",
+                    buying_price: 5000,
+                    selling_price: 5100,
+                     fournisseur: "Awa",
+                    categorie: "Lait",
+                    image: "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png",
+                    quantite: 2,
+                },
+                {
+                    id: 2,
+                   libelle: "ADA",
+                    code: "ABZ",
+                    buying_price: 5000,
+                    selling_price: 5100,
+                    fournisseur: "Awa",
+                    categorie: "Lait",
+                    image: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_1280.png",
+                    quantite: 1,
+                },
+                {
+                    id: 3,
+                    libelle: "PAWPAW",
+                    code: "ABZ",
+                    buying_price: 7000,
+                    selling_price: 6100,
+                    fournisseur: "Softcare",
+                    categorie: "Crème",
+                    image: null,
+                    quantite: 3,
+                },
+
+            ]
+        },
+        editItem(item) {
+            this.editedIndex = this.produits.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
+        deleteItem(item) {
+            this.editedItem = this.produits.indexOf(item)
+            this.editedIndex = Object.assign({}, item)
+            this.dialogDelete = true
+        },
+        deleteItemConfirm() {
+            this.produits.splice(this.editedIndex, 1)
+            this.closeDelete()
+        },
+        close() {
+            this.dialog = false
+            this.$nextTick(() => {
+                this.editedIndex = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+        save() {
+            if (this.editedIndex > -1) {
+                console.log(" this.editedItem : ", this.editedItem)
+                if (
+                    this.editedItem.fournisseur &&
+                    this.editedItem.fournisseur.hasOwnProperty('libelle') &&
+                    this.editedItem.categorie &&
+                     this.editedItem.categorie.hasOwnProperty('libelle') 
+                ) {
+                    let updatedObject = {
+                        ...this.editedItem,
+                        fournisseur: this.editedItem.fournisseur.libelle,
+                        categorie: this.editedItem.categorie.libelle
+                    }
+                    Object.assign(this.produits[this.editedIndex], updatedObject)
+                    // console.log('The key "state" exists in this.editedItem.statut.');
+                } else {
+                    Object.assign(this.produits[this.editedIndex], this.editedItem)
+                    //console.log('The key "state" does not exist in this.editedItem.statut.');
+                }
+            }
+            this.close()
+        },
+        getItem(fromPopup) {
+
+            if (
+                fromPopup.fournisseur &&
+                fromPopup.fournisseur.hasOwnProperty('libelle') &&
+                fromPopup.categorie &&
+                fromPopup.categorie.hasOwnProperty('libelle')
+            ) {
+                let updatedItem = {
+                    ...fromPopup,
+                     fournisseur: fromPopup.fournisseur.libelle,
+                    categorie: fromPopup.categorie.libelle
+                }
+                this.produits.push(updatedItem)
+            }
+        },
+        closeDelete() {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+    },
+    computed: {
+        produitLength() {
+            return this.produits.length;
+        },
+        formTitle() {
+            return this.editedIndex === -1 ? "Nouveau produit" : "Modifier un produit"
+        }
+    }
+}
 </script>
+<style scoped></style>
