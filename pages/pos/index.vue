3d81402b727c9062a395753afd19a89a1fdbec95
kaliloulah1155 <ibrahim.konate@ngser.com>
2023-12-21 14:21:29 +0000

    ui pos

--- pages/pos/index.vue
+++ pages/pos/index.vue
@@ -16,21 +16,26 @@
                     <template v-slot:default="{ items }">
                         <v-container class="pa-2" fluid>
                             <v-row dense>
-                                <v-col v-for="item in items" :key="item.title" cols="auto" md="4">
+                                <v-col v-for="item in items" :key="item.libelle" cols="auto" md="4">
                                     <v-card class="pb-3" border flat>
-                                        <v-img :src="item.raw.img" height="200" cover></v-img>
+                                        <v-img :src="item.raw.image" height="200" cover></v-img>
 
-                                        <v-list-item class="mb-2" :subtitle="item.raw.subtitle">
+                                        <v-list-item class="mb-2 text-center" :subtitle="`${item.raw.selling_price} F CFA`">
                                             <template v-slot:title>
-                                                <strong class="text-h6 mb-2">{{ item.raw.title }}</strong>
+                                                <strong class="text-h6 mb-2 ">{{ item.raw.libelle }}</strong>
                                             </template>
                                         </v-list-item>
 
                                         <div class="d-flex justify-space-between px-4">
                                             <div class="d-flex align-center text-caption text-medium-emphasis me-1">
-                                                <v-icon icon="clock" start></v-icon>
 
-                                                <div class="text-truncate">{{ item.raw.duration }}</div>
+                                                <div class="text-truncate">
+                                                    <v-badge :color="item.raw.quantite > 0 ? 'info' : 'error'" :content="item.raw.quantite" inline></v-badge>
+                                                    &nbsp;
+                                                    <v-chip :color="item.raw.quantite > 0 ? 'green' : 'red'">{{
+                                                        item.raw.statut }}</v-chip>
+
+                                                </div>
                                             </div>
 
                                             <v-btn border flat size="small" class="text-none" icon="add">
@@ -120,49 +125,46 @@
                     <v-col class="ma-1">
                         <v-row>
                             <v-col sm="12" md="4" lg="6" xl="8">
-                                 <span>TVA(%)</span>
+                                <span>TVA(%)</span>
                             </v-col>
                             <v-col sm="12" md="4" lg="6" xl="8">
-                               <v-text-field type="number" class=""  style="width:10rem;" />
+                                <v-text-field type="number" class="" style="width:10rem;" />
                             </v-col>
                         </v-row>
-                         <v-row>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                    REMISE(F CFA)
-                                </v-col>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                    <v-text-field type="number" class=""  style="width: 10rem;" />
-                                </v-col>
-                            </v-row>
-                             <v-row>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                    CLIENT
-                                </v-col>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                     <v-combobox  color="primary" variant="outlined"
-                                     clearable
-                                                  :items="clients" item-title="fullname"
-                                                item-value="id" return-object style="width:10rem;"></v-combobox>
-                                </v-col>
-                            </v-row>
-                              <v-row>
-                                    <v-col sm="12" md="4" lg="6" xl="8">
-                                        MOYEN DE PAIEMENT
-                                    </v-col>
-                                    <v-col sm="12" md="4" lg="6" xl="8">
-                                        <v-select  color="primary" variant="outlined"
-                                                      :items="moyen_payments" item-title="libelle"
-                                                    item-value="id" return-object style="width:10rem;"></v-select>
-                                    </v-col>
-                                </v-row>
-                             <v-row>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                    TOTAL
-                                </v-col>
-                                <v-col sm="12" md="4" lg="6" xl="8">
-                                    1 500 500 F CFA
-                                </v-col>
-                            </v-row>
+                        <v-row>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                REMISE(F CFA)
+                            </v-col>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                <v-text-field type="number" class="" style="width: 10rem;" />
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                CLIENT
+                            </v-col>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                <v-combobox color="primary" variant="outlined" clearable :items="clients"
+                                    item-title="fullname" item-value="id" return-object style="width:10rem;"></v-combobox>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                MOYEN DE PAIEMENT
+                            </v-col>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                <v-select color="primary" variant="outlined" :items="moyen_payments" item-title="libelle"
+                                    item-value="id" return-object style="width:10rem;"></v-select>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                TOTAL
+                            </v-col>
+                            <v-col sm="12" md="4" lg="6" xl="8">
+                                1 500 500 F CFA
+                            </v-col>
+                        </v-row>
 
                         <v-toolbar color="rgba(0,0,0,0)" flat class="d-flex justify-center">
                             <v-btn size="x-large" block variant="flat" color="green" density="compact"
@@ -191,10 +193,10 @@ export default {
         articles: [],
         clients: [
             { fullname: "KONATE Ibrahima", id: 1 },
-            { fullname: "KONE Issouf", id:2 },
+            { fullname: "KONE Issouf", id: 2 },
             { fullname: "KOUYATE Idriss", id: 3 },
         ],
-         moyen_payments: [
+        moyen_payments: [
             { libelle: "ESPECE", id: 1 },
             { libelle: "CHEQUE", id: 2 },
             { libelle: "CREDIT", id: 3 },
@@ -202,81 +204,81 @@ export default {
         ],
         produits: [
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/4.png',
-                title: 'The Sci-Fi Shooter Experience',
-                subtitle: 'Dive into a futuristic world of intense battles and alien encounters.',
-                advanced: false,
-                duration: '8 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/4.png',
+                libelle: "Lait",
+                selling_price: 2000,
+                quantite: 2,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/2.png',
-                title: 'Epic Adventures in Open Worlds',
-                subtitle: 'Embark on a journey through vast, immersive landscapes and quests.',
-                advanced: true,
-                duration: '10 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/2.png',
+                libelle: "Bonbon",
+                selling_price: 1000,
+                quantite: 0,
+                statut: 'En rupture',
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/3.png',
-                title: 'Surviving the Space Station Horror',
-                subtitle: 'Navigate a haunted space station in this chilling survival horror game.',
-                advanced: false,
-                duration: '9 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/3.png',
+                libelle: "Pomme",
+                selling_price: 2300,
+                quantite: 1,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/5.png',
-                title: 'Neon-Lit High-Speed Racing Thrills',
-                subtitle: 'Experience adrenaline-pumping races in a futuristic, neon-soaked city.',
-                advanced: true,
-                duration: '12 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/5.png',
+                libelle: "Ananas",
+                selling_price: 600,
+                quantite: 5,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/6.png',
-                title: 'Retro-Style Platformer Adventures',
-                subtitle: 'Jump and dash through pixelated worlds in this classic-style platformer.',
-                advanced: false,
-                duration: '11 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/6.png',
+                libelle: "Prume",
+                selling_price: 780,
+                quantite: 2,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/7.png',
-                title: 'Medieval Strategic War Campaigns',
-                subtitle: 'Lead armies into epic battles and conquer kingdoms in this strategic game.',
-                advanced: true,
-                duration: '10 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/7.png',
+                libelle: "Mangue",
+                selling_price: 850,
+                quantite: 3,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/1.png',
-                title: 'Underwater VR Exploration Adventure',
-                subtitle: 'Dive deep into the ocean and discover the mysteries of the underwater world.',
-                advanced: true,
-                duration: '11 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/1.png',
+                libelle: "Palme",
+                selling_price: 4120,
+                quantite: true,
+                statut: "En stock",
             },
             {
-                img: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/8.png',
-                title: '1920s Mystery Detective Chronicles',
-                subtitle: 'Solve crimes and uncover secrets in the glamourous 1920s era.',
-                advanced: false,
-                duration: '9 minutes',
+                image: 'https://cdn.vuetifyjs.com/docs/images/graphics/games/8.png',
+                libelle: "Oignon",
+                selling_price: 1250,
+                quantite: 2,
+                statut: "En stock",
             },
             {
-                img: '/img/bread.jpg',
-                title: 'Pain de miche',
-                subtitle: 'Solve crimes and uncover secrets in the glamourous 1920s era.',
-                advanced: false,
-                duration: '9 minutes',
+                image: '/img/bread.jpg',
+                libelle: 'Pain de miche',
+                selling_price: 200,
+                quantite: 1,
+                statut: "En stock",
             },
             {
-                img: '/img/croissant.jpg',
-                title: 'Croissant',
-                subtitle: 'Solve crimes and uncover secrets in the glamourous 1920s era.',
-                advanced: false,
-                duration: '9 minutes',
+                image: '/img/croissant.jpg',
+                libelle: 'Croissant',
+                selling_price: 1200,
+                quantite: 4,
+                statut: "En stock",
             },
             {
-                img: '/img/jaune_doeuf.jpg',
-                title: "Jaune d'oeuf",
-                subtitle: 'Solve crimes and uncover secrets in the glamourous 1920s era.',
-                advanced: false,
-                duration: '9 minutes',
+                image: '/img/jaune_doeuf.jpg',
+                libelle: "Jaune d'oeuf",
+                selling_price: 570,
+                quantite: 3,
+                statut: "En stock",
             },
         ],
     }),
