040466d0a42d86a847cd5de1c01cd55eed0268b0
Konate Ibrahima <ibrahim1155@outlook.com>
2024-07-19 11:18:32 +0000

    acces right

--- pages/salaires/index.vue
+++ pages/salaires/index.vue
@@ -1,135 +1,166 @@
 <template>
   <div class="dashboard ma-4">
-    <v-banner lines="one" color="warning">
-      <template v-slot:text>
-        <h1 class="text-subtitle-1 text-grey">Salaires</h1>
-      </template>
-    
+    <template v-if="accessRights.canView">
+      <v-banner lines="one" color="warning">
+        <template v-slot:text>
+          <h1 class="text-subtitle-1 text-grey">Salaires</h1>
+        </template>
 
-      <template v-if="accessRights.canCreate" v-slot:actions>
-        <popup-salaire @saveItem="getItem" :salaries="salaries" />
-      </template>
-    </v-banner>
-    <v-container class="my-5">
-      <v-card flat>
-        <v-card-title class="d-flex align-center pe-2">
-          <v-icon icon="view_list"></v-icon>&nbsp; Liste des salariés
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            prepend-inner-icon="search"
-            density="compact"
-            label="Rechercher un salarie"
-            single-line
-            flat
-            hide-details
-            variant="solo-filled"
-          ></v-text-field>
-        </v-card-title>
-        <v-divider class="mt-5"></v-divider>
-        <v-data-table
-          v-model:search="search"
-          :headers="headers"
-          :items="salaires"
-          :loading="salaireLength > 0 ? false : true"
-          :sort-by="[{ key: 'fullname', order: 'asc' }]"
-          items-per-page="10"
-        >
-          <template v-slot:top>
+        <template v-if="accessRights.canCreate" v-slot:actions>
+          <popup-salaire @saveItem="getItem" :salaries="salaries" />
+        </template>
+      </v-banner>
+      <v-container class="my-5">
+        <v-card flat>
+          <v-card-title class="d-flex align-center pe-2">
+            <v-icon icon="view_list"></v-icon>&nbsp; Liste des salariés
             <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-              <v-card>
-                <v-card-title>
-                  <span class="text-h5">{{ formTitle }}</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-form class="px-3" ref="form">
-                    <v-select
-                      label="Nom complet"
-                      class="mt-2"
-                      color="primary"
-                      variant="outlined"
-                      v-model="editedItem.fullname"
-                      :items="salaries"
-                      item-title="fullname"
-                      item-value="id"
-                      return-object
-                    ></v-select>
-                    <v-text-field
-                      label="Montant"
-                      class="mt-2"
-                      color="primary"
-                      clearable
-                      variant="outlined"
-                      v-model="editedItem.montant"
-                      :rules="montantRules"
-                    ></v-text-field>
-                    <v-text-field
-                      label="Date de paiement"
-                      class="mt-2"
-                      color="primary"
-                      clearable
-                      variant="outlined"
-                      v-model="editedItem.date_salaire"
-                      :rules="[isDateValid]"
-                    ></v-text-field>
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn variant="elevated" size="small" @click="close">
-                    Annuler
-                  </v-btn>
-                  <v-btn
-                    color="green-darken-3"
-                    variant="elevated"
-                    size="small"
-                    @click="save"
+            <v-text-field
+              v-model="search"
+              prepend-inner-icon="search"
+              density="compact"
+              label="Rechercher un salarie"
+              single-line
+              flat
+              hide-details
+              variant="solo-filled"
+            ></v-text-field>
+          </v-card-title>
+          <v-divider class="mt-5"></v-divider>
+          <v-data-table
+            v-model:search="search"
+            :headers="headers"
+            :items="salaires"
+            :loading="salaireLength > 0 ? false : true"
+            :sort-by="[{ key: 'fullname', order: 'asc' }]"
+            items-per-page="10"
+          >
+            <template v-slot:top>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="500px">
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-form class="px-3" ref="form">
+                      <v-select
+                        label="Nom complet"
+                        class="mt-2"
+                        color="primary"
+                        variant="outlined"
+                        v-model="editedItem.fullname"
+                        :items="salaries"
+                        item-title="fullname"
+                        item-value="id"
+                        return-object
+                      ></v-select>
+                      <v-text-field
+                        label="Montant"
+                        class="mt-2"
+                        color="primary"
+                        clearable
+                        variant="outlined"
+                        v-model="editedItem.montant"
+                        :rules="montantRules"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Date de paiement"
+                        class="mt-2"
+                        color="primary"
+                        clearable
+                        variant="outlined"
+                        v-model="editedItem.date_salaire"
+                        :rules="[isDateValid]"
+                      ></v-text-field>
+                    </v-form>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn variant="elevated" size="small" @click="close">
+                      Annuler
+                    </v-btn>
+                    <v-btn
+                      color="green-darken-3"
+                      variant="elevated"
+                      size="small"
+                      @click="save"
+                    >
+                      Valider
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card max-height="500px">
+                  <div class="text-center mt-4">
+                    <v-icon
+                      icon="delete_forever"
+                      color="red"
+                      size="large"
+                    ></v-icon>
+                  </div>
+                  <v-card-title class="text-h5 text-center"
+                    >&Ecirc;tes-vous s&ucirc;re de supprimer ?</v-card-title
                   >
-                    Valider
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card max-height="500px">
-                <div class="text-center mt-4">
-                  <v-icon
-                    icon="delete_forever"
-                    color="red"
-                    size="large"
-                  ></v-icon>
-                </div>
-                <v-card-title class="text-h5 text-center"
-                  >&Ecirc;tes-vous s&ucirc;re de supprimer ?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn variant="elevated" size="x-small" @click="closeDelete"
-                    >Annuler</v-btn
-                  >
-                  <v-btn
-                    color="red-darken-3"
-                    size="x-small"
-                    variant="elevated"
-                    @click="deleteItemConfirm"
-                    >Confirmer</v-btn
-                  >
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </template>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      variant="elevated"
+                      size="x-small"
+                      @click="closeDelete"
+                      >Annuler</v-btn
+                    >
+                    <v-btn
+                      color="red-darken-3"
+                      size="x-small"
+                      variant="elevated"
+                      @click="deleteItemConfirm"
+                      >Confirmer</v-btn
+                    >
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </template>
 
-          <template v-slot:item.actions="{ item }">
-            <v-icon v-if="accessRights.canEdit"  size="small" class="me-2" @click="editItem(item)">
-              edit_note
-            </v-icon>
-            <v-icon v-if="accessRights.canDelete" size="small" @click="deleteItem(item)"> delete </v-icon>
-          </template>
-        </v-data-table>
-      </v-card>
-    </v-container>
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                v-if="accessRights.canEdit"
+                size="small"
+                class="me-2"
+                @click="editItem(item)"
+              >
+                edit_note
+              </v-icon>
+              <v-icon
+                v-if="accessRights.canDelete"
+                size="small"
+                @click="deleteItem(item)"
+              >
+                delete
+              </v-icon>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-container>
+    </template>
+    <template v-else>
+      <v-sheet
+        class="d-flex align-center justify-center mt-4 flex-wrap text-center mx-auto px-4"
+        elevation="4"
+        height="250"
+        max-width="800"
+        width="100%"
+        rounded
+      >
+        <div>
+          <h2 class="text-h4 font-weight-black text-orange">
+            Vous n'avez pas les droits necessaires
+          </h2>
+        </div>
+      </v-sheet>
+    </template>
   </div>
   <v-snackbar
     v-model="snackbar"
@@ -157,41 +188,15 @@ export default {
     const { data, token } = useAuth();
 
     const accessRights = reactive({
+       profil_id:data.value.profil_id,
       canCreate: false,
       canView: false,
       canEdit: false,
       canDelete: false,
     });
 
-    const checkUserAccess = async () => {
-      let checkjson = {
-        profil_id: data.value.profil_id,
-        menu_libelle: "SALAIRES",
-      };
-      if (token.value) {
-        const response = await useNuxtApp().$axios.post(
-          `${useRuntimeConfig().public.apiBase}/test_permission`,
-          checkjson,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `${token.value}`,
-            },
-          }
-        );
-        console.log(response.data[1].add);
-        
-        accessRights.canView = response.data[0].show;
-        accessRights.canCreate = response.data[1].add;
-        accessRights.canEdit = response.data[2].edit;
-        accessRights.canDelete = response.data[4].supp;
-      }
-    };
-
-    onMounted(() => {
-      checkUserAccess();
-    });
-
+  
+     
     return { authStore, salaireStore, token, accessRights };
   },
   data: () => ({
@@ -255,8 +260,33 @@ export default {
   created() {
     this.initialize();
     this.employes_list();
+    this.accessRights.canView=true;
+    this.checkUserAccess();
+
   },
   methods: {
+    async checkUserAccess(){
+      let checkjson = {
+        profil_id: this.accessRights.profil_id,
+        menu_libelle: "SALAIRES",
+      };
+      if (this.token) {
+        const response = await useNuxtApp().$axios.post(
+          `${this.url}/test_permission`,
+          checkjson,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `${this.token}`,
+            },
+          }
+        );
+        this.accessRights.canView = response.data[0].show;
+        this.accessRights.canCreate = response.data[1].add;
+        this.accessRights.canEdit = response.data[2].edit;
+        this.accessRights.canDelete = response.data[4].supp;
+      }
+    },
     async initialize() {
       if (this.token) {
         const response = await useNuxtApp().$axios.get(`${this.url}/salaires`, {
