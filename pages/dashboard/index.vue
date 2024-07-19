170bc65dcd612c8079e9617b9a1f7b55b16cf42b
afisuyussuf <afisuyussuf370@gmail.com>
2024-07-19 09:46:13 +0000

    front

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -5,15 +5,56 @@
                 <h1 class="text-subtitle-1 text-grey">Dashboard</h1>
             </template>
         </v-banner>
-        <v-container class="my-5">
-              
-           
-        </v-container>
+        <v-container fluid>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-card class=""  color="blue lighten-4" dark>
+              <v-card-title>
+                Nombre de ventes
+              </v-card-title>
+              <v-card-title>
+                14
+              </v-card-title>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-card class=""  color="green lighten-4" dark>
+              <v-card-title>
+                Nombre de ventes
+              </v-card-title>
+              <v-card-title>
+                14
+              </v-card-title>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-card class=""  color="red lighten-4" dark>
+              <v-card-title>
+                Nombre de ventes
+              </v-card-title>
+              <v-card-title>
+                14
+              </v-card-title>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-card class=""  color="blue lighten-4" dark>
+              <v-card-title>
+                Nombre de ventes
+              </v-card-title>
+              <v-card-title>
+                14
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
 </template>
+
 <script setup>
 
-import {useAuthStore} from '../../stores/auth'
+import { useAuthStore } from '../../stores/auth'
  
 definePageMeta({
     middleware: 'auth',
@@ -31,3 +72,28 @@ if(status.value =="unauthenticated"){
 
 
 </script>
+
+
+<style>
+.v-card {
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.v-card-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+}
+
+.v-card-subtitle {
+  text-align: center;
+  font-size: 20px;
+}
+
+.v-card-text {
+  text-align: center;
+  font-size: 16px;
+}
+</style>
