b6a43bdae173785c3f3870e3cc99e981c233eabf
Konate Ibrahima <ibrahim1155@outlook.com>
2024-07-25 15:34:20 +0000

    dash

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -52,19 +52,21 @@
       </v-col>
       <!-- Carte pour l'augmentation des ventes -->
       <v-col cols="12" md="3">
-        <v-card  
+        <v-card
           class="mx-auto h-100"
           color="primary"
           rounded="xl"
           variant="tonal"
           elevation="3"
           hover
-           href="/depenses"
-          >
+          href="/depenses"
+        >
           <template v-slot:title>
-            <div class="font-weight-bold text-h5 text-black text-center">Dépenses</div>
+            <div class="font-weight-bold text-h5 text-black text-center">
+              Dépenses
+            </div>
           </template>
-         
+
           <template v-slot:text>
             <div class="float-left text-black mt-3">
               <div class="text-subtitle-1">
@@ -76,29 +78,30 @@
                 </v-badge>
                 )
               </div>
-             
             </div>
             <div class="float-right text-black mt-3">
-              <div class="text-h5 mt-5"> {{ moneyFormat(2000)}} F CFA </div>
-               </div>
+              <div class="text-h5 mt-5">{{ moneyFormat(2000) }} F CFA</div>
+            </div>
           </template>
         </v-card>
       </v-col>
       <!-- Carte pour le nombre total de clients -->
       <v-col cols="12" md="3">
-        <v-card  
+        <v-card
           class="mx-auto h-100"
           color="error"
           rounded="xl"
           variant="tonal"
           elevation="3"
           hover
-           href="/salaires"
-          >
+          href="/salaires"
+        >
           <template v-slot:title>
-            <div class="font-weight-bold text-h5 text-black text-center">Salaires</div>
+            <div class="font-weight-bold text-h5 text-black text-center">
+              Salaires
+            </div>
           </template>
-         
+
           <template v-slot:text>
             <div class="float-left text-black mt-3">
               <div class="text-subtitle-1">
@@ -110,34 +113,58 @@
                 </v-badge>
                 )
               </div>
-             
             </div>
             <div class="float-right text-black mt-3">
-              <div class="text-h5 mt-3 pb-3"> {{ moneyFormat(300000)}} F CFA </div>
-               </div>
+              <div class="text-h5 mt-3 pb-3">
+                {{ moneyFormat(300000) }} F CFA
+              </div>
+            </div>
           </template>
         </v-card>
       </v-col>
       <!-- Carte pour le nombre de nouveaux utilisateurs -->
       <v-col cols="12" md="3">
-        <v-card  
+        <v-card
           class="mx-auto h-100"
           color="#00FF00"
           rounded="xl"
           variant="tonal"
           elevation="3"
-          
-          >
+        >
           <template v-slot:title>
-            <div class="font-weight-bold text-h5 text-black text-center">Bénéfice</div>
+            <div class="font-weight-bold text-h5 text-black text-center">
+              Bénéfice
+            </div>
           </template>
-         
+
           <template v-slot:text>
             <div class="text-center text-black mt-3">
-              <div class="text-h5 mt-5"> {{ moneyFormat(500000)}} F CFA </div>
-               </div>
+              <div class="text-h5 mt-5">{{ moneyFormat(500000) }} F CFA</div>
+            </div>
           </template>
         </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center">
+      <v-col cols="6" md="3">
+        <v-text-field
+          v-model="dt_debut"
+          type="date"
+          variant="outlined"
+          clearable
+          class="w-2"
+        >
+          <template v-slot:label>
+            <span class="dt-custom-label text-h6">Date de début</span>
+          </template>
+        </v-text-field>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-text-field v-model="dt_fin" type="date" variant="outlined" clearable>
+          <template v-slot:label>
+            <span class="dt-custom-label text-h6">Date de fin</span>
+          </template>
+        </v-text-field>
       </v-col>
     </v-row>
   </v-container>
@@ -174,16 +201,18 @@ definePageMeta({
 
 //Get Session of user
 const authStore = useAuthStore();
-const { data, status } = useAuth();
+const { status } = useAuth();
 
 if (status.value == "unauthenticated") {
   authStore.data = {};
 }
+
+const dt_debut = ref(null);
+const dt_fin = ref(null);
 </script>
 
 <style scoped>
- 
-.custom-badge{
-  font-size: 1rem!important;
+.custom-badge {
+  font-size: 1rem !important;
 }
 </style>
