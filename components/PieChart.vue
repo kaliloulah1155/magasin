e66fd12875a6143556230a015f9a1471d82eebbf
Konate Ibrahima <ibrahim1155@outlook.com>
2024-07-25 22:24:05 +0000

    chart donut

--- components/PieChart.vue
+++ components/PieChart.vue
@@ -1,23 +1,41 @@
 <template>
-  <apexchart  type="pie" :options="chartOptions" :series="series"></apexchart>
+  <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
 </template>
 
 <script setup>
 const chartOptions = {
   chart: {
-    width: '100', // Pour s'assurer que le graphique occupe la largeur du conteneur
-    height: '100', // Ajustez la hauteur en fonction de vos besoins
+    type: "donut",
+    width: "100", // Pour s'assurer que le graphique occupe la largeur du conteneur
+    height: "100", // Ajustez la hauteur en fonction de vos besoins
   },
   labels: [
-    "Team 1", "Team 2", "Team 3", "Team 4", "Team 5",
-    "Team 6", "Team 7", "Team 8", "Team 9", "Team 10"
+    'Apple', 'Mango', 'Orange', 'Watermelon', 'Watermelon',
+'Pomme', 'Mangue', 'Mandarine', 'Banane', 'Parfum'
   ],
- 
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          position: "bottom",
+        },
+      },
+    },
+  ],
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          show: true,
+        },
+      },
+    },
+  },
 };
 
-const series = [
-  44, 55, 13, 43, 22,
-  33, 3, 10, 89, 2
-];
-
+const series = [44, 55, 13, 43, 22, 33, 3, 10, 89, 2];
 </script>
